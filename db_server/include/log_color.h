855b7ff96f9673dc6e77740d3a762ddf096a9b8c
Prakhar <mywolfdale@gmail.com>
2016-07-17 11:34:24 +0530

    Log Color Code

--- db_server/include/log_color.h
+++ db_server/include/log_color.h
@@ -1,10 +1,15 @@
 #ifndef lOG_COLOR
 #define LOG_COLOR
 
+#include<string>
 
-
-
-
+namespace Color {
+    const std::string red = "\033[22;31m";
+    const std::string yellow = "\033[22;33m";
+    const std::string green = "\033[22;32m";
+    const std::string magenta = "\033[22;35m";
+    const std::string reset = "\033[0m";
+};
 
 #endif 
 
