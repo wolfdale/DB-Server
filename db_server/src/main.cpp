da40872c1d04bf068a8409f264334444a83a6d5a
Prakhar <mywolfdale@gmail.com>
2016-07-17 16:05:38 +0530

    src with log trace

--- db_server/src/main.cpp
+++ db_server/src/main.cpp
@@ -1,42 +1,56 @@
 #include	"db_header.h"
 #include	"socket_class.h"
 #include	"daemon_class.h"
+#include	"logger.h"
 
 int main(int argc, char **argv)
 {
-	//Application log
-	std::cout << "DataBase Server Version 0.1"  << std::endl;
-	
-	std::cout << "Creating Database Server Socket" << std::endl;
-	
+	Logger logger;
 	Daemon db_deamon_svr;
-
 	Socket db_svr_sock;
 	int sock_fd;
-	//log to create socket
-	std::cout << "Creating Socket" << std::endl;
+	
+	logger.log_toconsole(Severity::INFO, 
+		std::vector<std::string>{"DataBase Server Version 0.1"});
+	logger.log_tostream(Severity::INFO, 
+		std::vector<std::string>{"DataBase Server Version 0.1"});
+	
+	logger.log_toconsole(Severity::INFO, 
+		std::vector<std::string>{"Creating Database Server Socket..."});
+	logger.log_tostream(Severity::INFO, 
+		std::vector<std::string>{"Creating DataBase Server Socket..."});
+
 	try{
 		sock_fd = db_svr_sock.create_socket();
-		std::cout << "Socket Created" << std::endl;
+		logger.log_toconsole(Severity::INFO, 
+			std::vector<std::string>{"Socket Created Successfully.."});
+		logger.log_tostream(Severity::INFO, 
+			std::vector<std::string>{"Socket Created Successfully.."});
 	}
-	catch( const char *exp_msg){
-		//log_msg
-		std::cout << exp_msg  << std::endl;
+	catch(const char *exp_msg){
+		logger.log_toconsole(Severity::ERROR, 
+			std::vector<std::string>{"Socket Failed. Database Shutdown !"});
+		logger.log_tostream(Severity::ERROR, 
+			std::vector<std::string>{"Socket Failed. Database Shutdown !"});
 		return EXIT_FAILURE;
 	}
-	//lOG deamon invoke
 	try{
+		logger.log_toconsole(Severity::INFO, 
+			std::vector<std::string>{"Invoking Database as Daemon Process..."});
+		logger.log_tostream(Severity::INFO, 
+			std::vector<std::string>{"Invoking Database as Daemon Process..."});
 		db_deamon_svr.start_daemon_process(sock_fd);			
 		
 	}
-	catch( const char *exp_msg)
+	catch(const char *exp_msg)
 	{
-		//log msg
-		std::cout << exp_msg << std::endl;
+		logger.log_toconsole(Severity::ERROR, 
+			std::vector<std::string>{"Daemon Module Failed. Database Shutdown !"});
+		logger.log_tostream(Severity::ERROR, 
+			std::vector<std::string>{"Daemon Module Failed. Database Shutdown !"});
 		return EXIT_FAILURE;
 	}
-
-	return EXIT_FAILURE;
+	return EXIT_SUCCESS;
 
 }
 
