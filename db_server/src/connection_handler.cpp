f7f96e8136f430e66230b9009ed24cdd8babdf56
Prakhar <mywolfdale@gmail.com>
2016-07-18 22:34:10 +0530

    Algorithm Proposal

--- db_server/src/connection_handler.cpp
+++ db_server/src/connection_handler.cpp
@@ -10,6 +10,23 @@ void ConnectionHandler::db_connection_handler(int db_sock_fd)
 				(struct sockaddr*)&clientaddr, &length);
 		
 		std::cout << query_string << std::endl;
+	
+		//Proposed Algorithm for Working
+		//
+		//First create a Session ID ( for client )
+		//create thread arguments for threads [ Buffer size for comm. , 
+		//		status = OK ]
+		// Create a port address for communication.
+		// Detach Thread
+		// Send the status report to client in thread 
+		//
+		// Receive a request to create a table
+		// make all necessary arrangements.
+		// send status OK
+		//  
+		// Create two files on Database [Metadata] [actual data]
+
+
 		
 		if (query_sting[0] != "$")
 		{
