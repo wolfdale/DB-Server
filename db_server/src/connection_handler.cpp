aa8b861663f16f096903d0a197686ea35e173f2b
Prakhar <mywolfdale@gmail.com>
2016-07-19 22:05:55 +0530

    Modification

--- db_server/src/connection_handler.cpp
+++ db_server/src/connection_handler.cpp
@@ -26,17 +26,6 @@ void ConnectionHandler::db_connection_handler(int db_sock_fd)
 		//  
 		// Create two files on Database [Metadata] [actual data]
 
-
-		
-		if (query_sting[0] != "$")
-		{
-			//Log Special Query
-			// Display Databaase status 
-		}
-		else
-		{
-			// decode_query(query_string);		
-		}
 	}
 
 
