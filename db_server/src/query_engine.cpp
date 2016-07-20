c0afb1d32ea18497955fbbb38d5ff952443359b8
Prakhar <mywolfdale@gmail.com>
2016-07-20 22:55:51 +0530

    Proposed Algorithm For Query engine

---
+++ db_server/src/query_engine.cpp
@@ -0,0 +1,13 @@
+
+//Query Engine Parser
+//Algorithm Proposed
+//Connection handler will pass query to query engine
+//query engine will parse the client query 
+//send it to the Database engine.
+//database engine will perform file IO.
+//results are send to Dispatcher to Send result to client.
+
+
+
+
+
