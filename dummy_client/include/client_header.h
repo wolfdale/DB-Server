f40ff2c5d0a9cadcff3a445b4be4dc7494a18e47
Prakhar <mywolfdale@gmail.com>
2016-07-23 21:07:20 +0530

    Initial Check Point Reached

--- dummy_client/include/client_header.h
+++ dummy_client/include/client_header.h
@@ -2,8 +2,18 @@
 #define GLOBAL_CLIENT_HEADER
 
 	#include<iostream>
-	#include<
+	#include<vector>
+	#include<sys/types.h>
+	#include<sys/socket.h>
+	#include<netinet/in.h>
+	#include<arpa/inet.h>
+	#include<sys/wait.h>
+	#include<fcntl.h>
+	#include<unistd.h>
+	#include<cstdlib>
+	#include<cstring>
 
+	#define CHECK 0
 
 
 
