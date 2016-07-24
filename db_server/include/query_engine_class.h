315ca22628b70254b80722b50de7e3265c0c0aab
Prakhar <mywolfdale@gmail.com>
2016-07-24 11:06:24 +0530

    Query Engine Class

---
+++ db_server/include/query_engine_class.h
@@ -0,0 +1,31 @@
+#ifndef QUERY_ENGINE
+#define QUERY_ENGINE
+
+class QueryEngine{
+
+private:
+	//Query Hash Table
+
+public:
+	bool query_validator(string);
+	vector<string> spliter(string, char); 
+
+
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
