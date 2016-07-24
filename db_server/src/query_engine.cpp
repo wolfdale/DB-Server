52a54161aeb57145c4c509c99c1150248a645fb1
Prakhar <mywolfdale@gmail.com>
2016-07-24 10:59:06 +0530

    Initial Draft of Query Engine

--- db_server/src/query_engine.cpp
+++ db_server/src/query_engine.cpp
@@ -1,3 +1,5 @@
+#include "db_header.h"
+#include "logger.h"
 
 //Query Engine Parser
 //Algorithm Proposed
@@ -7,6 +9,40 @@
 //database engine will perform file IO.
 //results are send to Dispatcher to Send result to client.
 
+
+// Query Validation/Parsing -> pass to DB Engine -> Pass to Dispatcher -> 
+//							Send status to client 
+
+// QUERY Structure - 
+//
+//	$INIT_CONN$ -- Server will Send OK status with Buffer Size
+//	
+//	$CREATE_T$<T_NAME> -- Server Make Metadata File & 
+//			      Datafile<named after table> 
+//
+//	Data type of each col is -- <String>
+//	$ADD_SCH$<NO_OF_COL>$<Name_of_Col>$ .. < > 
+//
+//	$ADD_DATA$<Data Wit COL_MAPPING>
+//	
+//
+
+bool QueryEngine :: query_validator(string query)
+{
+		
+
+
+
+
+}
+
+vector<string> QueryEngine :: spliter(string query, char delim)
+{
+	
+
+
+
+}
 
 
 
